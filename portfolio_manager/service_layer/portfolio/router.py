ebf60f15948130d5692f317b73aeec14c83b0f94
Jakub Wysocki <kuba.w108@gmail.com>
2023-06-30 22:10:53 +0200

    Add new function for portfolio managment

--- portfolio_manager/service_layer/portfolio/router.py
+++ portfolio_manager/service_layer/portfolio/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from portfolio_manager.bootstrap import get_bootstrap, Bootstrap
-from portfolio_manager.service_layer.portfolio.schemas import PortfolioSchema, PortfoliosSchema, OrderSchema, CreatePortfolioSchema
+from portfolio_manager.service_layer.portfolio.schemas import PortfolioSchema, PortfoliosSchema, OrderSchema, CreatePortfolioSchema, CashScema, PortfolioIdSchema
 from portfolio_manager.shared.schemas import SuccessSchema
 from portfolio_manager.domain.models import PositionOrder
 
@@ -59,3 +59,53 @@ async def create_order(
     if not result:
         raise HTTPException(status_code=400, detail="Order not created")
     return {"success": result}
+
+@router.post("/add-cash/",status_code=200)
+async def add_cash(
+    args: CashScema,
+    bootstrap: Bootstrap = Depends(get_bootstrap),
+) -> SuccessSchema:
+    portfolio = await bootstrap.portfolio_repository.get_one(args.portfolio_id)
+
+    if portfolio is None:
+        raise HTTPException(status_code=404, detail="Portfolio not found")
+
+    portfolio.cash += args.cash
+    # TODO save to DB here
+    return {"success": True}
+
+@router.post("/pay-out-cash/",status_code=200)
+async def pay_out_cash(
+    args: CashScema,
+    bootstrap: Bootstrap = Depends(get_bootstrap),
+) -> SuccessSchema:
+    portfolio = await bootstrap.portfolio_repository.get_one(args.portfolio_id)
+
+    if portfolio is None:
+        raise HTTPException(status_code=404, detail="Portfolio not found")
+
+    portfolio.cash -= args.cash
+    # TODO save to DB here
+    return {"success": True}
+
+
+@router.delete("/orders/",status_code=200)
+async def delete_orders(
+    args: PortfolioIdSchema,
+    bootstrap: Bootstrap = Depends(get_bootstrap),
+) -> SuccessSchema:
+    portfolio = await bootstrap.portfolio_repository.get_one(args.portfolio_id)
+
+    if portfolio is None:
+        raise HTTPException(status_code=404, detail="Portfolio not found")
+
+    cash_from_orders = 0.0
+    for order in portfolio.orders:
+        cash_from_orders += order.max_price * order.shares
+    
+    portfolio.orders = []
+
+    portfolio.cash -= cash_from_orders
+
+    # TODO save to DB here
+    return {"success": True}
